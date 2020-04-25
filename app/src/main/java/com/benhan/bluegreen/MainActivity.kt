4169063a18a5857d401b0614bf3724baef9185b2
My name <dkfmaek333@gmail.com>
2020-04-25 21:59:38 +0900

    intent

--- app/src/main/java/com/benhan/bluegreen/MainActivity.kt
+++ app/src/main/java/com/benhan/bluegreen/MainActivity.kt
@@ -1,13 +1,51 @@
 package com.benhan.bluegreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+    val tvRegister: TextView = findViewById(R.id.tvRegister)
+    val tvLogin: TextView = findViewById(R.id.tvLogin)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+
+        tvRegister.setOnClickListener {
+            onRegisterClicked()
+        }
+
+        tvLogin.setOnClickListener {
+            onLoginClicked()
+        }
+
+
+
+
+
+    }
+
+
+    fun onRegisterClicked(){
+
+        val intent = Intent(this, LoginActivity::class.java)
+
+        startActivity(intent)
+
+    }
+
+    fun onLoginClicked(){
+
+        val intent = Intent(this, LoginActivity2::class.java)
+
+        startActivity(intent)
     }
 }
